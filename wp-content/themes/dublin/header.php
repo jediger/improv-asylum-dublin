31217933091c5526f8caafa84ad6c6b0378de90e
John Ediger <jediger@racepointglobal.com>
2014-04-03 09:16:36 -0400

    removed viewport from header

--- wp-content/themes/dublin/header.php
+++ wp-content/themes/dublin/header.php
@@ -9,7 +9,6 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<link href='http://fonts.googleapis.com/css?family=Fjalla+One|Muli' rel='stylesheet' type='text/css'>
 		<?php wp_head(); ?>
